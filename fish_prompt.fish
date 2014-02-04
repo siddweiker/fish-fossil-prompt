b9341f47c91d2070cf79e8484e0adc640a9b6a43
siddweiker <siddweiker@gmail.com>
2014-02-03 21:34:49 -0500

    Added colors for state changes and message for extra files.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -27,9 +27,29 @@ function fish_prompt --description 'Write out the prompt'
 end
 
 function  __fish_fossil_prompt --description "Prompt function for Fossil"
+    set -l yellow (set_color -o yellow)
+    set -l blue (set_color -o blue)
+    set -l green (set_color -o green)
+    set -l gray (set_color -o black)
+    set -l normal (set_color -o normal)
+
+    set -l info ""
     set -l branch (command fossil branch ls 2^/dev/null | awk '/\* /{print $2}')
+    set -l changes (command fossil changes 2^/dev/null)
+    set -l extra (count (command fossil extra 2^/dev/null))
+    set -l col "$normal"
+
+    if test -n "$changes"
+        set col "$yellow"
+    else
+        set col "$blue"
+    end
+
+    if test $extra -ne 0
+        set info "$gray|$yellow+$extra"
+    end
     
-    if test $branch
-        printf '(%s)' $branch
+    if test -n "$branch"
+        printf "(%s%s%s%s)" $col $branch $info $normal
     end
 end
